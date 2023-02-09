ce7ed67b635fa09165ef92259b0f40221e3d5ce7
alvarezjuanign <alvarez.juanign@gmail.com>
2023-02-09 17:47:55 -0300

    Adding check items and clear all funcionality button

--- To-do_list/main.js
+++ To-do_list/main.js
@@ -1,16 +1,31 @@
 const button = document.querySelector(".btn")
+const clearAll = document.querySelector(".clear")
 const newItem = document.getElementById("new-item")
 const list = document.querySelector(".list")
+const item = document.querySelectorAll('li')
 
 function appendItem (){
-    const item = document.createElement('li')
-    item.appendChild(document.createTextNode(newItem.value))
-    list.appendChild(item)
+    if (newItem.value === '') return
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(newItem.value))
+    list.appendChild(li)
 }
+
+list.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked')
+    }
+})
 
 button.addEventListener('click', ()=>{
     appendItem(newItem.value)
     newItem.value = ''
+})
+
+clearAll.addEventListener('click', ()=>{
+    while (list.firstChild){
+        list.removeChild(list.firstChild)
+    }
 })
 
 window.addEventListener('keyup', (e)=>{
