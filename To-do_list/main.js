7be9270ba538ef3bd326904dbbc11a1b33112d50
alvarezjuanign <alvarez.juanign@gmail.com>
2023-02-13 20:28:18 -0300

    Finishing the functionality of to do list

--- To-do_list/main.js
+++ To-do_list/main.js
@@ -1,45 +1,43 @@
-const button = document.querySelector(".btn")
-const clearAll = document.querySelector(".clear")
-const newItem = document.getElementById("new-item")
-const list = document.querySelector(".list")
-let item = document.getElementsByTagName('LI')
+let input = document.querySelector("#new-item");
+let btn = document.querySelector(".btn");
+let list = document.querySelector(".list");
 
-function appendItem (){
-    if (newItem.value === '') return
 
-    let li = document.createElement('li')
-    li.appendChild(document.createTextNode(newItem.value))
-    list.appendChild(li)
+function addUIItem(txt) {
+    let li = document.createElement("li");
+    li.innerHTML = txt;
+    list.insertBefore(li, list.childNodes[0])
+    const delBtn = document.createElement("button")
+    delBtn.textContent = "x";
+    delBtn.classList.add("delete")
+    li.appendChild(delBtn)
+    
+    delBtn.addEventListener("click", (e) => {
+        li.parentNode.removeChild(li)
+        savedTasks = savedTasks.filter((e) => e !== txt)
+        localStorage.setItem("tasks", JSON.stringify(savedTasks))
+    });
 }
 
-function clearAllItems(){
-    while (list.firstChild){
-        list.removeChild(list.firstChild)
+
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || []
+savedTasks.forEach(addUIItem)
+
+btn.addEventListener("click", () => {
+    let txt = input.value
+    if (txt === "" || txt === ' ') {
+        alert("Please write something to do!")
+    } else {
+        savedTasks.push(txt);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks))
+        addUIItem(txt)
+        input.value = ""
     }
-}
+});
 
-list.addEventListener('click', (e)=>{
-    if(e.target.tagName === 'LI'){
-        e.target.classList.toggle('checked')
-    }
-})
-
-button.addEventListener('click', ()=>{
-    appendItem()
-    newItem.value = ''
-})
-
-clearAll.addEventListener('click', ()=>{
-    clearAllItems()
-})
-
-window.addEventListener('keyup', (e)=>{
-    if (e.key === 'Enter'){
-    appendItem()
-    newItem.value = ''
-    }
-
-    if(e.key === 'Escape'){
-        clearAllItems()
+list.addEventListener("click", (e) => {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("checked")
     }
 })
+
